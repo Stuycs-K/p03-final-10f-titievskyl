a13f582930ac9416ea7b7878839f3162ecc1b778
LT-Nacl <139832384+LT-Nacl@users.noreply.github.com>
2026-01-08 19:54:13 -0500

    Switched to SDL2 for the test
    A lot of stolen boilerplate :(

--- util/SDL3COL.c
+++ util/SDL3COL.c
@@ -1,26 +1,56 @@
 #include <SDL2/SDL.h>
 
-void drawColumn(SDL_Renderer *r, float x, float y1, float y2) {
-    SDL_RenderLine(r, x, y1, x, y2);
+void drawColumn(SDL_Renderer *r, int x, int y1, int y2) {
+    SDL_RenderDrawLine(r, x, y1, x, y2);
 }
 
 int main(int argc, char *argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Columns", 640, 480, 0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL, 0);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Columns",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+
+    if (!renderer) {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
     SDL_Event e;
     int running = 1;
 
     while (running) {
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_EVENT_QUIT) running = 0;
+            if (e.type == SDL_QUIT) {
+                running = 0;
+            }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-        for (float x = 50; x < 640; x += 50) {
+        for (int x = 50; x < 640; x += 50) {
             drawColumn(renderer, x, 100, 380);
         }
 
